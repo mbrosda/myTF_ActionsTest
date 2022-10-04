d098be50ddf6ff13ecd0a6314c2334d8633716b7
mbrosda <60555402+mbrosda@users.noreply.github.com>
2022-10-04 12:20:59 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,8 @@
 terraform {
   backend "azurerm" {
-    #resource_group_name  = "arm-tfstate"
     storage_account_name = "tfsmbrdev"
     container_name       = "mytfactionstest"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
-
-    #access_key = "A59DONvK1h/3cj5PQLCckDcrwZkfi3yZ7wwoNe5SivliqcsDmA1InmVqlf4AUbo8tsqb2Y+81q8x3ktAJg4U9Q=="
   }
 }
