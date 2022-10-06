12ce44b9339213e16499f05dee63de7d4a016212
Marcus Brosda <60555402+mbrosda@users.noreply.github.com>
2022-10-06 21:42:14 +0200

    storage account

--- 0120-StorAccts.tf
+++ 0120-StorAccts.tf
@@ -1,7 +1,7 @@
-# resource "azurerm_storage_account" "storacct1" {
-#   name                                           = "mytfactionsstoracct${var.Environment}"
-#   resource_group_name                            = resource.azurerm_resource_group.mytf_actions_rg.name
-#   location                                       = resource.azurerm_resource_group.mytf_actions_rg.location
-#   account_tier                                   = "Standard"
-#   account_replication_type                       = "LRS"
-# }
+resource "azurerm_storage_account" "storacct1" {
+  name                                           = "mytfactionsstoracct${var.Environment}"
+  resource_group_name                            = resource.azurerm_resource_group.mytf_actions_rg.name
+  location                                       = resource.azurerm_resource_group.mytf_actions_rg.location
+  account_tier                                   = "Standard"
+  account_replication_type                       = "LRS"
+}
